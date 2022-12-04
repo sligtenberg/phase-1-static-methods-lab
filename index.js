ea23b57fdc3bfb7c07781589a0d2d565b7331ece
stephen ligtenberg <bountyhunter24@gmail.com>
2022-12-04 03:19:14 -0500

    commit tree b188acc2c9a6ed89753a0fe461d0479c6cd4fdd0

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+  static capitalize(input) {
+    return input[0].toUpperCase() + input.slice(1)
+  }
+  static sanitize(input) {
+    return input.replace(/[^A-Za-z0-9-' ]+/g, "")
+  }
+  static titleize(input) {
+    const capFrstLetter = this.capitalize(input)
+    let lowerCaseWords = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let title = []
+    let wordsArray = capFrstLetter.split(" ")
+    wordsArray.forEach(word => {
+      if (lowerCaseWords.includes(word)) title.push(word)
+      else title.push(this.capitalize(word))
+    })
+    return title.join(" ")
+  }
 }
+
+console.log(Formatter.capitalize("stephen"))
